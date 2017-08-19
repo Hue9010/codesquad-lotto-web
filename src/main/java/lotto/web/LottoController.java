d95d580d0196d227f1a1c6b1412b93c44e463635
LHS <fthr56@gmail.com>
2017-08-20 04:08:30 +0900

    아마존 e2c 및 라즈베리파이에서 빌드시 오류가 발생하여 맥북서 정상 작동하는 코드 재업로드

--- src/main/java/lotto/web/LottoController.java
+++ src/main/java/lotto/web/LottoController.java
@@ -12,22 +12,30 @@ public class LottoController {
 	Machine machine;
 	User user;
 
-	@GetMapping("/lotto")
-	public ModelAndView showLotto(int inputMoney) {
-		ModelAndView mav = new ModelAndView("lotto/show");
-
+	@GetMapping("/buyLotto")
+	public String buyLotto(int inputMoney) {
 		machine = new Machine();
 		user = new User(inputMoney);
 		user.buyLottos(machine);
+		return "/showLotto";
+	}
+	@GetMapping("/showLotto")
+	public ModelAndView showLotto() {
+		ModelAndView mav = new ModelAndView("lotto/show");
 		mav.addObject("buyLottos", user.buyLottos);
 		mav.addObject("volume", user.money.lottoVolume());
 		return mav;
 	}
 
 	@GetMapping("/matchLotto")
-	public ModelAndView matchLotto(String winningNumber) {
-		ModelAndView mav = new ModelAndView("lotto/result");
+	public String matchLotto(String winningNumber) {
 		user.matchLottos(machine, winningNumber);
+		return "/result";
+	}
+	
+	@GetMapping("/result")
+	public ModelAndView showResult() {
+		ModelAndView mav = new ModelAndView("lotto/result");
 		mav.addObject("result", user.result);
 		mav.addObject("rate", user.calculateRate());
 		return mav;
